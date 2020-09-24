8834a943bc6a32fcc308cc2fe6aab0ecee41eae5
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-09-23 23:01:46 -0400

    Create getbook.py

---
+++ getbook.py
@@ -0,0 +1,25 @@
+import pymongo
+import requests
+import json
+
+
+def addBook(isbn):
+    r = requests.get(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}')
+    bookJSON = r.json()['items'][0]['volumeInfo']
+
+    title = bookJSON['title']
+    try:
+        year = int(bookJSON['publishedDate'])
+    except:
+        year = 'NA'
+    
+    author = bookJSON['authors'][0]
+    description = bookJSON['description']
+
+    client = pymongo.MongoClient('mongodb+srv://katskiUser:y2fqe3XBba27PeA@folio-database.4q0uf.azure.mongodb.net/default')
+    db = client['default']
+    col = db['books']
+    entry = {'title': title, 'year': year, 'author': author, 'description': description}
+    col.insert_one(entry)
+
+addBook('9780393325997')
